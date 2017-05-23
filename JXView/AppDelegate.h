dc25d879849a096728520de5fddf390703acb8f8
dujinxin <dujinxin8866@163.com>
2017-05-23 10:07:19 +0800

    Initial Commit

---
+++ JXView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JXView
+//
+//  Created by 杜进新 on 2017/5/23.
+//  Copyright © 2017年 杜进新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JXView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXView
+//
+//  Created by 杜进新 on 2017/5/23.
+//  Copyright © 2017年 杜进新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
